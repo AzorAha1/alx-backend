94e9fc0a2a317dd238bb824c3efb3ef6e53429a6
AzorAha1 <wizfaiz@icloud.com>
2024-05-08 10:47:54 +0100

    3-lru_cache.py

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""
+
+Keyword arguments:
+argument -- description
+Return: return_description
+"""
+
+
+from collections import OrderedDict
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """_summary_
+
+    Args:
+        BaseCaching (_type_): _description_
+    """
+    def __init__(self):
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """put
+
+        Args:
+            key (_type_): _description_
+            item (_type_): _description_
+        """
+        if key is None or item is None:
+            return
+        if key in self.cache_data:
+            self.cache_data.move_to_end(key)
+        self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            discard_key, _ = self.cache_data.popitem(last=False)
+            print(f'DISCARD: {discard_key}')
+
+    def get(self, key):
+        if key is None or key not in self.cache_data:
+            return None
+        if key in self.cache_data:
+            self.cache_data.move_to_end(key)
+        return self.cache_data[key]
