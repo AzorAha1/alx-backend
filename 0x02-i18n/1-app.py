cf050f08d8f3a98fa133871548090a399be05273
AzorAha1 <wizfaiz@icloud.com>
2024-05-14 17:54:37 +0100

    1-app.py

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""this is for flask"""
+
+from flask_babel import Babel
+app = __import__('0-app').app
+babel = Babel(app)
+
+
+class Config:
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
