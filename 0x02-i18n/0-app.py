ba15571587cd743bec9ba845c9c724f9a037d786
AzorAha1 <wizfaiz@icloud.com>
2024-05-14 17:36:53 +0100

    0-app.py

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -8,6 +8,11 @@ from flask import render_template
 app = Flask(__name__)
 @app.route('/')
 def index():
+    """_summary_
+
+    Returns:
+        _type_: _description_
+    """
     return render_template('0-index.html')
 
 
