12f0dcf54900b7c9f337e722a832687db778aaac
AzorAha1 <wizfaiz@icloud.com>
2024-05-17 21:59:15 +0100

    2-app.py

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """this is for flask"""
 
-from flask import request
+from flask import render_template, request
 from flask_babel import Babel
 app = __import__('0-app').app
 babel = Babel(app)
@@ -11,3 +11,17 @@ babel = Babel(app)
 def get_locale():
     """locale selector to determine preferred language"""
     request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index2():
+    """_summary_
+
+    Returns:
+        _type_: _description_
+    """
+    return render_template('2-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
